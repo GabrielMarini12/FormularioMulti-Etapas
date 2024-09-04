f00aadbd7c6aa98d154afba94e019fbeeb4e03a9
Gabriel Marini <gabrielmarini12@icloud.com>
2024-09-04 18:55:26 -0300

    exportanto os contextos necessários e passando o router dentro do meu formProvider.

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import { router } from "./App";
 import "./index.css";
 
 import { RouterProvider } from "react-router-dom";
+import { FormProvider } from "./contexts/FormContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <FormProvider>
+      <RouterProvider router={router} />
+    </FormProvider>
   </React.StrictMode>
 );
