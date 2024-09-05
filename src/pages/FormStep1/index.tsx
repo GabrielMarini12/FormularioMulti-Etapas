0f28f2f5f5086e54ebd1961126285fb5b16bb653
Gabriel Marini <gabrielmarini12@icloud.com>
2024-09-05 20:36:47 -0300

    verificando se o nome for preenchido, pois só vai permitir ir ao passo 2 se o usuário tiver digitado seu nome.

--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -16,7 +16,11 @@ export const FormSep1 = () => {
   }, []);
 
   const handleNextStep = () => {
-    navigate("/step2");
+    if (state.name !== "") {
+      navigate("/step2");
+    } else {
+      alert("Preencha os dados!");
+    }
   };
 
   const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
