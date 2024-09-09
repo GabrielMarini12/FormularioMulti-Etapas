35742763c025749a4464b557ae4e88123ec22931
Gabriel Marini <gabrielmarini12@icloud.com>
2024-09-09 13:29:02 -0300

    iniciando o layout da página do passo 2

--- src/pages/FormStep2/index.tsx
+++ src/pages/FormStep2/index.tsx
@@ -1,5 +1,39 @@
+import { useNavigate } from "react-router-dom";
 import * as C from "./styles";
+import { useForm, FormActions } from "../../contexts/FormContext";
+import { Theme } from "../../components/Theme";
+import { useEffect } from "react";
 
 export const FormSep2 = () => {
-  return <C.Container>Passo 2</C.Container>;
+  const navigate = useNavigate();
+  const { state, dispatch } = useForm();
+
+  useEffect(() => {
+    dispatch({
+      type: FormActions.setCurrentStep,
+      payload: 2,
+    });
+  }, []);
+
+  const handleNextStep = () => {
+    if (state.name !== "") {
+      navigate("/step2");
+    } else {
+      alert("Preencha os dados!");
+    }
+  };
+
+  return (
+    <Theme>
+      <C.Container>
+        <p>Passo 2/3</p>
+        <h1>Vamos começar com seu nome</h1>
+        <p>Preencha o campo a baixo com seu nome completo</p>
+
+        <hr />
+
+        <button onClick={handleNextStep}>Próximo</button>
+      </C.Container>
+    </Theme>
+  );
 };
