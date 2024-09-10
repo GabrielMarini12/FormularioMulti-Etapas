c81f9672e968f623be59755e3fbba5ec42b2493f
Gabriel Marini <gabrielmarini12@icloud.com>
2024-09-10 11:49:27 -0300

    iniciando o layout e fucionalidades do sidebar.

---
+++ src/components/SidebarItem/styles.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  margin: 30px 0;
+  cursor: pointer;
+
+  a {
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+  }
+`;
+
+export const Info = styled.div``;
+
+export const Title = styled.div``;
+
+export const Description = styled.div``;
+
+export const IconArea = styled.div``;
+
+export const Point = styled.div``;
